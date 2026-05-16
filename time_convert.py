bfa375015756c26677c7ee0917100e5955a163d8
mun9uk-ship-it <mun9uk@gmail.com>
2026-05-16 11:26:10 +0100

    Update time_convert.py
    v01

--- time_convert.py
+++ time_convert.py
@@ -1,1 +1,8 @@
+t = int(input("Input the time in seconds: "))
 
+hours = t // 3600
+minutes = (t % 3600) // 60
+seconds = t % 60
+
+# print result
+print("Time is:", hours, ":", minutes, ":", seconds)
